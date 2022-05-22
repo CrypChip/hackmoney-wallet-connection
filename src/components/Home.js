98f34c615dc73ade7bb1abc242c284c037f5d187
gyan0890 <gyanlakshmi@gmail.com>
2022-05-22 19:45:47 +0530

    Committing Wallet code

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+// components/Homey.js
+
+import React from "react";
+import {
+    VStack,
+    useDisclosure,
+    Button,
+    Text,
+    HStack,
+    Select,
+    Input,
+    Box
+  } from "@chakra-ui/react";
+
+const Homey = () => (
+  <div>
+    <Button> Create a group </Button>
+    <Button> Add an expense </Button> 
+  </div>
+);
+
+export default Homey;
---
+++ src/networks.js
@@ -0,0 +1,30 @@
+export const networkParams = {
+  "0x63564c40": {
+    chainId: "0x63564c40",
+    rpcUrls: ["https://api.harmony.one"],
+    chainName: "Harmony Mainnet",
+    nativeCurrency: { name: "ONE", decimals: 18, symbol: "ONE" },
+    blockExplorerUrls: ["https://explorer.harmony.one"],
+    iconUrls: ["https://harmonynews.one/wp-content/uploads/2019/11/slfdjs.png"]
+  },
+  "0xa4ec": {
+    chainId: "0xa4ec",
+    rpcUrls: ["https://forno.celo.org"],
+    chainName: "Celo Mainnet",
+    nativeCurrency: { name: "CELO", decimals: 18, symbol: "CELO" },
+    blockExplorerUrl: ["https://explorer.celo.org"],
+    iconUrls: [
+      "https://celo.org/images/marketplace-icons/icon-celo-CELO-color-f.svg"
+    ]
+  },
+  "0x116e9": {
+    chainId: "0x116e9",
+    rpcUrls: ["https://godwoken-testnet-v1.ckbapp.dev"],
+    chainName: "Nervos L2 Testnet",
+    nativeCurrency: { name: "CKB", decimals: 18, symbol: "CKB" },
+    blockExplorerUrl: ["https://v1.betanet.gwscan.com"],
+    iconUrls: [
+      "https://celo.org/images/marketplace-icons/icon-celo-CELO-color-f.svg"
+    ]
+  }
+};
